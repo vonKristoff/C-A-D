4c263fa5e1a8381c22b9aed1e05150ae98efbe0d
Vonkristoff <mrjcnicolas@gmail.com>
2016-02-04 17:58:41 +0000

    multi canvas test success

--- js/app.js
+++ js/app.js
@@ -1,64 +1,72 @@
-document.body.appendChild(CAD.create({ width: 500, height: 500}).element());
 
-CAD.plot('axis', function (cx, cy) {
-    this.draw()
-        .setStyles({
-            stroke: {
-                width: .5, color: "rgba(0,0,0,.5)"
+function loop () {
+
+    CAD.plot('axis', function (cx, cy) {
+        this.draw()
+            .setStyles({
+                stroke: {
+                    width: .5, color: "rgba(0,0,0,.5)"
+                }
+            })
+            .startPath(cx, 0.5)
+            .defineSegments([
+                { lineTo: [cx, 500.5] }
+            ])
+            .endPath({
+                stroke: true, fill: false
+            })
+            .startPath(0.5, cy)
+            .defineSegments([
+                { lineTo: [500.5, cy] }
+            ])
+            .endPath({
+                stroke: true
+            })
+        .done();
+    })
+    .print('axis', [250.5, 250.5])
+    .draw()
+        .pushState()
+        .setStyles({fill: {color: 'red'}})
+        .transform({translate: {x:250, y:250}, rotate:.3})
+        .rect({
+            fill: [-25,-25,50,50]
+        })
+        .popState()
+        .shape({
+            type: 'rect',
+            style: {fill: {color: '#0f9'}},
+            coords: {x: 250, y: 250},
+            dimensions: {w: 20, h:20},
+            transform: {
+                rotate: Math.random(),
+                center: true
             }
         })
-        .startPath(cx, 0.5)
-        .defineSegments([
-            { lineTo: [cx, 500.5] }
-        ])
-        .endPath({
-            stroke: true, fill: false
-        })
-        .startPath(0.5, cy)
-        .defineSegments([
-            { lineTo: [500.5, cy] }
-        ])
-        .endPath({
-            stroke: true
+    .done()
+    .bitmap().create('virtual-bitmap', { width:10, height: 10 })
+    .use('virtual-bitmap', function () {
+        console.log(this.raw.canvas);
+        this.draw()
+            .setStyles({
+                fill: { color: "red" }
+            }).rect({
+                fill: [0, 0, 5, 5]
+            }).setStyles({
+                fill: { color: "purple" }
+            }).rect({
+                fill: [5, 5, 5, 5]
+            })
+        .done()
+    }).done()
+    .grid().build('standard',500,500,20)
+        .use(function (item, collection, index) {
+            this.getContext().drawImage(this.getVirtualCanvas('virtual-bitmap'), item.x, item.y);
         })
     .done();
-})
-.print('axis', [250.5, 250.5])
-.draw()
-    .pushState()
-    .setStyles({fill: {color: 'red'}})
-    .transform({translate: {x:250, y:250}, rotate:.3})
-    .rect({
-        fill: [-25,-25,50,50]
-    })
-    .popState()
-    .shape({
-        type: 'circ',
-        style: {fill: {color: '#0f9'}},
-        coords: {x: 250, y: 250},
-        dimensions: {w: 20, h:20},
-        transform: {
-            center: true
-        }
-    })
-.done()
-.bitmap().create('virtual-bitmap', { width:10, height: 10 })
-.use('virtual-bitmap', function () {
-    this.draw()
-        .setStyles({
-            fill: { color: "red" }
-        }).rect({
-            fill: [0, 0, 5, 5]
-        }).setStyles({
-            fill: { color: "purple" }
-        }).rect({
-            fill: [5, 5, 5, 5]
-        })
-    .done()
-}).done()
-.grid().build('standard',500,500,20)
-    .use(function (item, collection, index) {
-        this.getContext().drawImage(this.getVirtualCanvas('virtual-bitmap'), item.x, item.y);
-    })
-.done();
+}
+
+document.body.appendChild(CAD.create({ width: 500, height: 500}).element());
+loop();
+
 
